51b51fb2bd61a2d31e901ec1eb39f91508a1de30
arduinobymyself <arduinobymyself@gmail.com>
2023-01-02 09:43:58 -0300

    REmovendo arquivos executaveis, resposicionando arquivos

---
+++ ProgramacaoDescomplicada/LinguagemC/padrao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+	
+*/
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+
+
+
+
+	printf("\n\n");
+	//system("pause");
+	return 0;
+}
